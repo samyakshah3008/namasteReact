c13201f3ef7662e3916722e39847e5c9e4bbf3ae
Samyak Shah <samyakshah3008@gmail.com>
2023-06-06 16:28:55 +0530

    Lectureone updated content

---
+++ lectureone/app.js
@@ -0,0 +1,24 @@
+const childDiv = React.createElement(
+  "div",
+  {
+    id: "child",
+  },
+  "Child Div 1"
+);
+const childDiv2 = React.createElement(
+  "div",
+  {
+    id: "child",
+  },
+  "Child Div 2"
+);
+const parentDiv = React.createElement(
+  "div",
+  {
+    id: "parent-div",
+  },
+  [childDiv, childDiv2]
+);
+const rootID = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootID);
+root.render(parentDiv);
